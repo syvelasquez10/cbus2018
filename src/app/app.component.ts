b5c133ef2f40a72d52e6b7f75dd733dc3c7593a4
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-04-16 11:50:48 -0500

    cambio por areas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,13 @@ export class AppComponent {
   title = 'cambiosCBU2018';
   artesTipoA = false;
   artesTipoB = false;
+  artesTipoB2 = false;
   socialesTipoA = false;
   socialesTipoB = false;
+  socialesTipoB2 = false;
   tecTipoA = false;
   tecTipoB = false;
+  tecTipoB2 = false;
   contador = 0;
 
   getCreditosActuales() {
@@ -23,10 +26,16 @@ export class AppComponent {
     if (this.artesTipoB) {
       this.contador++;
     }
+    if (this.artesTipoB2) {
+      this.contador++;
+    }
     if (this.socialesTipoA) {
       this.contador++;
     }
     if (this.socialesTipoB) {
+      this.contador++;
+    }
+    if (this.socialesTipoB2) {
       this.contador++;
     }
     if (this.tecTipoA) {
@@ -35,10 +44,14 @@ export class AppComponent {
     if (this.tecTipoB) {
       this.contador++;
     }
+    if (this.tecTipoB2) {
+      this.contador++;
+    }
 
     this.contador = this.contador * 3;
     return this.contador;
   }
+
 
   getCreditosFaltantes() {
     if (this.getCreditosActuales() <= 9) {
@@ -51,18 +64,69 @@ export class AppComponent {
   }
 
   unoPorTipo() {
-    if ((this.artesTipoA || this.artesTipoB) && (this.socialesTipoA || this.socialesTipoB) && (this.tecTipoA || this.tecTipoB)) {
+    if (this.artesCompleto() && this.socialesCompleto() && this.tecCompleto()) {
       return true;
     } else {
       return false;
     }
   }
 
+  artesCompleto() {
+    if (this.artesTipoA || this.artesTipoB || this.artesTipoB2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  socialesCompleto() {
+    if (this.socialesTipoA || this.socialesTipoB || this.socialesTipoB2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  tecCompleto() {
+    if (this.tecTipoA || this.tecTipoB || this.tecTipoB2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  areaFaltante() {
+    if (this.getCreditosActuales() <= 6) {
+      return 'cualquier área';
+    } else if (this.getCreditosActuales() === 9) {
+      if (this.unoPorTipo()) {
+        return 'cualquier área';
+      } else {
+       if (this.artesCompleto()) {
+        return 'Pensamiento Científico';
+       } else {
+        return 'Culturas, Artes y Humanidades';
+       }
+      }
+    } else if (this.getCreditosActuales() >= 12) {
+      if (this.unoPorTipo()) {
+        return 'cualquier área';
+      } else {
+       if (this.artesCompleto()) {
+        return 'Pensamiento Científico';
+       } else {
+        return 'Culturas, Artes y Humanidades';
+       }
+      }
+    }
+  }
+
   cursosRestantes() {
     if (this.getCreditosActuales() <= 6) {
       return Math.floor(4 / (this.getCreditosActuales() / 3));
-    } else if (this.getCreditosActuales() <= 9 && !this.unoPorTipo()) {
-      // Y si tengo un curso de cada área entonces 0?
+    } else if (this.getCreditosActuales() <= 9) {
+      return 1;
+    } else if (!this.unoPorTipo()) {
       return 1;
     } else {
       return 0;
